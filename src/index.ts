0095f25fe53eff4f741a401eff65f241b80dbded
maronnjapan <tihoutaikai20@gmail.com>
2025-01-13 12:12:20 +0900

    ロガーの設定

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
 import { registerTodoRoutes } from './tasks/controller'
 import { prismaMiddleware } from './middlewares/init-prisma-client.middleware'
+import { logger } from 'hono/logger'
+import { requestId } from 'hono/request-id'
+import { loggerMiddleware } from './middlewares/logger.middleware'
 
 type Bindings = {
   DB: D1Database
   // MY_KV_NAMESPACE: KVNamespace
 }
 const app = new OpenAPIHono<{ Bindings: Bindings }>()
+
+app.use(logger())
+app.use('*', requestId())
+app.use('*', loggerMiddleware)
 app.use('*', prismaMiddleware)
 registerTodoRoutes(app)
+
 
 app.doc('/doc', {
   openapi: '3.0.0',
